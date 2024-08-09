593d804f7bbc16a814fac166e99b64d5c9023bd1
Iris Esmeralda Gómez <iris.e.gomez@proton.me>
2024-08-09 11:14:03 -0300

    Solve day 9 from 2015

---
+++ puzzles/2015/day09/main.go
@@ -0,0 +1,174 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	input, err := os.ReadFile("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	lines := strings.Split(strings.TrimSpace(string(input)), "\n")
+
+	fmt.Printf("Part 1 solution: %d\n", part1(lines))
+	fmt.Printf("Part 2 solution: %d\n", part2(lines))
+}
+
+func part1(lines []string) int {
+	locations := make(map[string]int)
+	for _, line := range lines {
+		split := strings.Split(line, " ")
+
+		_, ok := locations[split[0]]
+		if !ok {
+			locations[split[0]] = len(locations)
+		}
+
+		_, ok = locations[split[2]]
+		if !ok {
+			locations[split[2]] = len(locations)
+		}
+	}
+
+	matrix := make([][]int, len(locations))
+	for i := range matrix {
+		matrix[i] = make([]int, len(locations))
+	}
+
+	for _, line := range lines {
+		split := strings.Split(line, " ")
+
+		weight, err := strconv.Atoi(split[4])
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		matrix[locations[split[0]]][locations[split[2]]] = weight
+		matrix[locations[split[2]]][locations[split[0]]] = weight
+	}
+
+	return shortestPath(matrix)
+}
+
+func part2(lines []string) int {
+	locations := make(map[string]int)
+	for _, line := range lines {
+		split := strings.Split(line, " ")
+
+		_, ok := locations[split[0]]
+		if !ok {
+			locations[split[0]] = len(locations)
+		}
+
+		_, ok = locations[split[2]]
+		if !ok {
+			locations[split[2]] = len(locations)
+		}
+	}
+
+	matrix := make([][]int, len(locations))
+	for i := range matrix {
+		matrix[i] = make([]int, len(locations))
+	}
+
+	for _, line := range lines {
+		split := strings.Split(line, " ")
+
+		weight, err := strconv.Atoi(split[4])
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		matrix[locations[split[0]]][locations[split[2]]] = weight
+		matrix[locations[split[2]]][locations[split[0]]] = weight
+	}
+
+	return longestPath(matrix)
+}
+
+func shortestPath(matrix [][]int) int {
+	minWeight := math.MaxInt
+
+	for source := 0; source < len(matrix); source++ {
+		var vertices []int
+		for i := 0; i < len(matrix); i++ {
+			if i != source {
+				vertices = append(vertices, i)
+			}
+		}
+
+		for _, permutation := range permutations(vertices) {
+			currentWeight := 0
+
+			j := source
+			for _, i := range permutation {
+				currentWeight += matrix[j][i]
+				j = i
+			}
+
+			minWeight = min(minWeight, currentWeight)
+		}
+	}
+
+	return minWeight
+}
+
+func longestPath(matrix [][]int) int {
+	maxWeight := 0
+
+	for source := 0; source < len(matrix); source++ {
+		var vertices []int
+		for i := 0; i < len(matrix); i++ {
+			if i != source {
+				vertices = append(vertices, i)
+			}
+		}
+
+		for _, permutation := range permutations(vertices) {
+			currentWeight := 0
+
+			j := source
+			for _, i := range permutation {
+				currentWeight += matrix[j][i]
+				j = i
+			}
+
+			maxWeight = max(maxWeight, currentWeight)
+		}
+	}
+
+	return maxWeight
+}
+
+func permutations(a []int) [][]int {
+	var heap func(int, []int)
+	result := [][]int{}
+
+	heap = func(k int, a []int) {
+		if k == 1 {
+			temp := make([]int, len(a))
+			copy(temp, a)
+			result = append(result, temp)
+		} else {
+			heap(k-1, a)
+
+			for i := 0; i < k-1; i++ {
+				if k%2 == 0 {
+					a[i], a[k-1] = a[k-1], a[i]
+				} else {
+					a[0], a[k-1] = a[k-1], a[0]
+				}
+				heap(k-1, a)
+			}
+		}
+	}
+	heap(len(a), a)
+
+	return result
+}
